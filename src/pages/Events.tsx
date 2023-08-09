29b774d255ecedb8aac14a1ef7680855b37b0e08
Tewson Seeoun <tewson.seeoun@gmail.com>
2023-08-09 08:10:46 +0100

    ssr

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
-
 import { EventCard, type Event } from "../components/EventCard";
-import type { StrapiNode } from "../types";
 
-export function Events() {
-  const [events, setEvents] = useState<Event[]>();
-  useEffect(() => {
-    fetch("http://localhost:1337/api/events")
-      .then((response) => response.json())
-      .then((response) => response.data as StrapiNode<Event>[])
-      .then((eventNodes) =>
-        setEvents(eventNodes.map((eventNode) => eventNode.attributes))
-      );
-  }, []);
-  console.log({ events });
+export function Events({ events }: { events: Event[] }) {
   return (
     <div>
-      {events?.map((event) => (
+      {events.map((event) => (
         <EventCard event={event} />
       ))}
     </div>
