2b2c6851d3a7833529320757e1b70bcf15cf442d
Tewson Seeoun <tewson.seeoun@gmail.com>
2023-08-09 08:32:53 +0100

    Show publisher and times

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,13 +1,45 @@
+interface Publisher {
+  username: string;
+}
+
 export interface Event {
   title: string;
   startTime: string;
   endTime: string;
+  publisher: {
+    data: {
+      id: number;
+      attributes: Publisher;
+    };
+  };
 }
+
+const dateFormatter = new Intl.DateTimeFormat("en-IE", {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  hour12: false,
+});
 
 export function EventCard({ event }: { event: Event }) {
   return (
-    <div>
-      <strong>{event.title}</strong>
+    <div
+      style={{
+        borderWidth: 1,
+        borderColor: "black",
+        borderStyle: "solid",
+        padding: 20,
+      }}
+    >
+      <h1>{event.title}</h1>
+      <h2>{event.publisher.data.attributes.username}</h2>
+      <h2>
+        {dateFormatter.format(new Date(event.startTime))} -{" "}
+        {dateFormatter.format(new Date(event.endTime))}
+      </h2>
     </div>
   );
 }
