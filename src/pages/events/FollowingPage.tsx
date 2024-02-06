dc952490e5849f0ea1cb0f0b5a2fbe456d1ea078
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-02-06 19:35:11 +0000

    Move follower data to localStorage

--- src/pages/events/FollowingPage.tsx
+++ src/pages/events/FollowingPage.tsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
 import { Events } from "../../components/Events";
 import axios from "axios";
+import qs from "qs";
+import { getFollowedPublishers } from "../../utils/localStorage";
 
 export function FollowingPage() {
   const [events, setEvents] = useState();
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      axios
-        .get(`${import.meta.env.PUBLIC_API_PREFIX}users/me/following-events`, {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
+    const followedPublishers = getFollowedPublishers();
+    const query = qs.stringify(
+      {
+        populate: "*",
+        filters: {
+          id: {
+            $in: followedPublishers,
           },
+        },
+      },
+      {
+        encodeValuesOnly: true,
+      }
+    );
+    axios
+      .get(`${import.meta.env.PUBLIC_API_PREFIX}users?${query}`)
+      .then((response) =>
+        response.data.flatMap((publisherNode: any) => {
+          return publisherNode.events.map((eventNode: any) => ({
+            ...eventNode,
+            publisher: {
+              id: publisherNode.id,
+              username: publisherNode.username,
+            },
+          }));
         })
-        .then((response) => response.data)
-        .then((events) => setEvents(events));
-    }
+      )
+      .then((events) => setEvents(events));
   }, []);
+
   return (
     <>
       <h1>Events from organisations you follow</h1>
